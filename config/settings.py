c8e664a82ed003d7685c4e742a0b43c802a5b050
Varshamadhavan1403 <varshamadhavan1622@gmail.com>
2026-08-12 16:14:37 +0530

    Fix SECRET_KEY for GitHub Actions

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
  
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = SECRET_KEY = os.getenv("SECRET_KEY") or "django-insecure-github-actions-test-key"
 
 DEBUG = os.getenv("DEBUG") == "True"
 
